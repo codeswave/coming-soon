246d2fb0d030d5531ae8557722de5b65101d5b8c
Huzaifa <sarmad@xenex-media.com.au>
2023-06-18 10:57:30 +0500

    first commit

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { ComingSoonComponent } from './coming-soon/coming-soon.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: '/coming-soon',
+    pathMatch: 'full'
+  },
+  {
+    path: 'coming-soon',
+    component: ComingSoonComponent
+  },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule {
+
+
+ }
---
+++ src/app/coming-soon/coming-soon.component.ts
@@ -0,0 +1,38 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-coming-soon',
+  templateUrl: './coming-soon.component.html',
+  styleUrls: ['./coming-soon.component.scss']
+})
+export class ComingSoonComponent {
+  days:any;
+  hours:any;
+  minutes:any;
+  seconds:any;
+
+  constructor() {
+    const launchDate = new Date('2023-07-01'); // Set your launch date here
+    this.countdownTimer(launchDate);
+  }
+
+  countdownTimer(launchDate: Date) {
+    const timerInterval = setInterval(() => {
+      const currentTime = new Date().getTime();
+      const timeDifference = launchDate.getTime() - currentTime;
+
+      this.days = Math.floor(timeDifference / (1000 * 60 * 60 * 24));
+      this.hours = Math.floor((timeDifference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+      this.minutes = Math.floor((timeDifference % (1000 * 60 * 60)) / (1000 * 60));
+      this.seconds = Math.floor((timeDifference % (1000 * 60)) / 1000);
+
+      if (timeDifference < 0) {
+        clearInterval(timerInterval);
+        this.days = 0;
+        this.hours = 0;
+        this.minutes = 0;
+        this.seconds = 0;
+      }
+    }, 1000);
+  }
+}
