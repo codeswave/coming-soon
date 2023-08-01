a897cf4d6b955b089732b439d6781c6a14d33234
Huzaifa <huzaifa8300@gmail.com>
2023-08-01 12:43:41 +0500

    timer updated

--- src/app/coming-soon/coming-soon.component.ts
+++ src/app/coming-soon/coming-soon.component.ts
@@ -12,7 +12,7 @@ export class ComingSoonComponent {
   seconds:any;
 
   constructor() {
-    const launchDate = new Date('2023-08-01'); // Set your launch date here
+    const launchDate = new Date('2023-08-20'); // Set your launch date here
     this.countdownTimer(launchDate);
   }
 
